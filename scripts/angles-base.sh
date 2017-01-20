d01c4ae07be470ad8a4356bff87bf410d45e8f1d
Leandro Martinez <leandromartinez98@gmail.com>
2017-01-20 14:41:11 -0200

    Changed version file to 17.020

--- scripts/angles-base.sh
+++ scripts/angles-base.sh
@@ -10,7 +10,7 @@
 #
 #  L. Martinez, Institut Pasteur, Apr 22, 2008.
 #
-#  Version 17.018
+#  Version 17.020
 #
 # IMPORTANT:
 # Path for angles program:
--- src/common.f90
+++ src/common.f90
@@ -14,7 +14,7 @@ end module charsize
 subroutine version()
   
   write(*,*) 
-  write(*,*) ' Version 17.018 '
+  write(*,*) ' Version 17.020 '
   write(*,*) 
 
 end subroutine version
